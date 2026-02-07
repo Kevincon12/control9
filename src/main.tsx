580a2dab8dbad9b74a5a067a50867aa2830a69b3
Strelnikov Ilia <ily_strelnikov@mail.ru>
2026-02-07 11:13:22 +0600

    Инициализировали проект со всеми необходимыми библиотеками

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import {Provider} from "react-redux";
+import {BrowserRouter} from "react-router-dom";
+
+createRoot(document.getElementById('root')!).render(
+    <Provider store={store}>
+        <StrictMode>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </StrictMode>
+    </Provider>
+)
